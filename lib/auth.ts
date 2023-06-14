e0cfc53f6a3427f084a354c8999d30600102c211
joshuaarnaez-lop <joshua.arnaez@lophils.com>
2023-06-14 22:14:38 +0800

    added seed script and prisma configs

---
+++ lib/auth.ts
@@ -0,0 +1,73 @@
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "@/lib/prisma";
+import bcrypt from "bcrypt";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Sign in",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any) {
+        const { email, password } = credentials;
+        try {
+          if (!credentials?.email || !credentials.password) {
+            return null;
+          }
+          const user = await prisma.user.findFirst({
+            where: { email },
+            include: { profile: true },
+          });
+          // If user does not exist,
+          if (!user) return null;
+
+          // If user exist compare provided password and hashpassword
+          const comparePassword = bcrypt.compareSync(password, user.password);
+          if (!comparePassword) return null;
+
+          return user;
+        } catch (error: any) {
+          throw new Error(error.message);
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }: any) {
+      if (user) token.user = user;
+      return token;
+    },
+    async session({ session, token }: any) {
+      session.user = token.user;
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    maxAge: 60 * 60,
+  },
+  pages: {
+    signIn: "/",
+    signOut: "/",
+  },
+};
---
+++ prisma/seed.ts
@@ -0,0 +1,65 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+const seed = async () => {
+  try {
+    const users = [];
+
+    const password = await bcrypt.hash("password123", 10);
+
+    for (let i = 0; i < 10; i++) {
+      users.push({
+        username: `user${i + 1}`,
+        password,
+        email: `user${i + 1}@admin.com`,
+        role: "Admin",
+        profile: {
+          create: {
+            address: `Teacher Address ${i + 1}`,
+            mobileNumber: `Teacher Mobile Number ${i + 1}`,
+          },
+        },
+      });
+
+      users.push({
+        username: `user${i + 1}`,
+        password,
+        email: `user${i + 1}@teacher.com`,
+        role: "TEACHER",
+        profile: {
+          create: {
+            address: `Teacher Address ${i + 1}`,
+            mobileNumber: `Teacher Mobile Number ${i + 1}`,
+          },
+        },
+      });
+
+      users.push({
+        username: `user${i + 1}`,
+        password,
+        email: `user${i + 1}@officer.com`,
+        role: "OFFICER",
+        profile: {
+          create: {
+            address: `Teacher Address ${i + 1}`,
+            mobileNumber: `Teacher Mobile Number ${i + 1}`,
+          },
+        },
+      });
+    }
+
+    const createdUsers = await Promise.all(
+      users.map((user) => prisma.user.create({ data: user }))
+    );
+
+    console.log("Students created:", createdUsers);
+  } catch (error) {
+    console.error("Error seeding the database:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+seed();
