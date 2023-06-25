7bb50f8400a3f0d2a65562d6e4dc0e219f1739a8
joshuaarnaez-lop <joshua.arnaez@lophils.com>
2023-06-25 21:21:58 +0800

    branch josh

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
 import Image from "next/image";
-
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login");
+  }, [router]);
+
   return (
     <main className="">
       <h1>tester</h1>
